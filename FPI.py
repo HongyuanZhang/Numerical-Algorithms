8f4fff288df74b7f6fe3abbd857a5b50ea6beece
HongyuanZhang <zhanghon@grinnell.edu>
2019-11-17 19:24:53 -0600

    delete unnecessary code

--- FPI.py
+++ FPI.py
@@ -1,11 +1,7 @@
 '''
 Fixed Point Iteration Method for solving g(x)=x
 '''
-import math
 
-#example function g(x)
-def g(x):
-    return math.cos(x)**2
 
 #Fixed Point Iteration Method
 #Stopping Criteria: when num_step steps have been executed
@@ -15,5 +11,3 @@ def FPI(g, x0, num_step):
     for i in range(num_step):
         x = g(x)
     return x
-
-print(FPI(g, 1, 500))
